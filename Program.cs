ff072a58570243cc02c40fb4a0f36ebccd2ff038
seanpolid <seanpolidori0@gmail.com>
2024-01-19 01:54:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DirectoryTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        }
+    }
+}
