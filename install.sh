10392e1cb460dbfb969a402a05243ff72a446afb
seanpolid <seanpolidori0@gmail.com>
2024-01-19 02:38:18 -0500

    Updated installation

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
+# Change permission of release script
+chmod +x release.sh
+
 # Package the application
 dotnet publish
 
 # Copy the output to bin
 mkdir -p /opt/bin/csharp
 cp ./bin/Debug/net6.0/publish/* /opt/bin/csharp
-cp ./release.sh /opt/bin
+cp ./release.sh /opt/bin/release
